d35191a401cdb1f7b965be16251487e42d50b81a
Minin <70429604+MininxD@users.noreply.github.com>
2024-03-03 11:03:30 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -12,9 +12,12 @@ gsap.to(titleEl, {
 }
 
 
-export function scrambleTitle() {
 var abjad = ["A","B","C","D","E","F","0","1","2","3","4","5","6","7","8","9"];
 var abjadTotal = [...Array(16).keys()];
+
+
+
+export function scrambleTitle() {
 var scrambleText = document.getElementById("scrambleTitleEl");
 
 const acakHuruf = setInterval(acakan, 50);
@@ -31,16 +34,13 @@ setTimeout (function() {
 
 
 export function scrambleFooter() {
-  var abjadd = ["A","B","C","D","E","F","0","1","2","3","4","5","6","7","8","9"];
-var abjadTotall = [...Array(16).keys()];
 var scrambleFoot = document.getElementById("footerScramble");
-
 
 const acakHuruf = setInterval(acakan, 50);
 function acakan() {
-gsap.utils.shuffle(abjadTotall);
-//console.log(abjadd[abjadTotall[0]]);
-scrambleFoot.style.color = "#"+ abjadd[abjadTotall[0]] + abjadd[abjadTotall[1]] + abjadd[abjadTotall[2]] + abjadd[abjadTotall[3]] + abjadd[abjadTotall[4]] + abjadd[abjadTotall[5]] ;
-scrambleFoot.innerHTML = "#" + abjadd[abjadTotall[0]] + abjadd[abjadTotall[1]] + abjadd[abjadTotall[2]] + abjadd[abjadTotall[3]] + abjadd[abjadTotall[4]] + abjadd[abjadTotall[5]] ;
+gsap.utils.shuffle(abjadTotal);
+//console.log(abjad[abjadTotal[0]]);
+scrambleFoot.style.color = "#"+ abjad[abjadTotal[0]] + abjad[abjadTotal[1]] + abjad[abjadTotal[2]] + abjad[abjadTotal[3]] + abjad[abjadTotal[4]] + abjad[abjadTotal[5]] ;
+scrambleFoot.innerHTML = "#" + abjad[abjadTotal[0]] + abjad[abjadTotal[1]] + abjad[abjadTotal[2]] + abjad[abjadTotal[3]] + abjad[abjadTotal[4]] + abjad[abjadTotal[5]] ;
 }
 }
