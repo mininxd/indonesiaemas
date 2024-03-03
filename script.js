feee9befc56a764b90036eb1bf4135c5dc30b1a5
Minin <70429604+MininxD@users.noreply.github.com>
2024-03-03 21:22:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ gsap.to(titleEl, {
 
 
 var abjad = ["A","B","C","D","E","F","0","1","2","3","4","5","6","7","8","9"];
-var abjadTotal = [...Array(16).keys()];
+var abjadArr = [...Array(16).keys()];
 
 
 
@@ -22,14 +22,14 @@ var scrambleText = document.getElementById("scrambleTitleEl");
 
 const acakHuruf = setInterval(acakan, 50);
 function acakan() {
-gsap.utils.shuffle(abjadTotal);
-// console.log(abjad[abjadTotal[0]]);
-scrambleText.innerHTML = abjad[abjadTotal[0]] + abjad[abjadTotal[1]] + abjad[abjadTotal[2]] + abjad[abjadTotal[3]] + abjad[abjadTotal[4]] + abjad[abjadTotal[5]] ;
+gsap.utils.shuffle(abjadArr);
+// console.log(abjad[abjadArr[0]]);
+scrambleText.innerHTML = abjad[abjadArr[0]] + abjad[abjadArr[1]] + abjad[abjadArr[2]] + abjad[abjadArr[3]] + abjad[abjadArr[4]] + abjad[abjadArr[5]] ;
 }
 setTimeout (function() {
   scrambleText.style.display = "none";
   clearInterval(acakHuruf);
-}, 4000);
+}, 4500);
 }
 
 
@@ -38,12 +38,12 @@ var scrambleFoot = document.getElementById("footerScramble");
 var scrambleFoot2 = document.getElementById("footerScramble2");
 
 setInterval( function() {
-gsap.utils.shuffle(abjadTotal);
-var hexText = abjad[abjadTotal[0]] + abjad[abjadTotal[1]] + abjad[abjadTotal[2]] + abjad[abjadTotal[3]] + abjad[abjadTotal[4]] + abjad[abjadTotal[5]];
-//console.log(abjad[abjadTotal[0]]);
+gsap.utils.shuffle(abjadArr);
+var hexText = abjad[abjadArr[0]] + abjad[abjadArr[1]] + abjad[abjadArr[2]] + abjad[abjadArr[3]] + abjad[abjadArr[4]] + abjad[abjadArr[5]];
+//console.log(abjad[abjadArr[0]]);
 scrambleFoot.style.color = "#" + hexText;
 scrambleFoot.innerHTML = "#" + hexText;
 scrambleFoot2.innerHTML = "#" + hexText;
 titleEl.style.filter = "drop-shadow(0px 0px 3px #" + hexText + ")"
 },50);
-}
+ }
